f6246cdf12b41badec6c9840bceb2b9ea81c9325
Lautaro Matas <lmatas@gmail.com>
2023-02-24 22:19:21 +0100

    fix V4.1.1__Network_FK_update.sql

--- src/main/resources/db/migration/V4.1.1__Network_FK_update.sql
+++ src/main/resources/db/migration/V4.1.1__Network_FK_update.sql
@@ -12,23 +12,25 @@ ALTER TABLE network ADD CONSTRAINT fk_prevalidator FOREIGN KEY (pre_validator_id
 
 -- drop CONSTRAINT fkbbkgd5g0hj337vag34y4obfni (validator_id) if exists
 ALTER TABLE network DROP CONSTRAINT IF EXISTS fkbbkgd5g0hj337vag34y4obfni;
+ALTER TABLE network DROP CONSTRAINT IF EXISTS fk_validator;
 
 -- create constraint named fk_validator on network (validator_id) referencing validator (id) avoiding delete
-ALTER TABLE network ADD CONSTRAINT fk_validator FOREIGN KEY (id) REFERENCES validator(id) ON DELETE NO ACTION;
+ALTER TABLE network ADD CONSTRAINT fk_validator FOREIGN KEY (validator_id) REFERENCES validator(id) ON DELETE NO ACTION;
 
 
 -- drop CONSTRAINT fkd2dwqqyr0lkyvmmo12vy2poru (secondary_transformer_id) if exists
 ALTER TABLE network DROP CONSTRAINT IF EXISTS fkd2dwqqyr0lkyvmmo12vy2poru;
+ALTER TABLE network DROP CONSTRAINT IF EXISTS fk_secondary_transformer;
 
 -- create constraint named fk_secondary_transformer on network (secondary_transformer_id) referencing transformer (id) avoiding delete
-ALTER TABLE network ADD CONSTRAINT fk_secondary_transformer FOREIGN KEY (id) REFERENCES transformer(id) ON DELETE NO ACTION;
+ALTER TABLE network ADD CONSTRAINT fk_secondary_transformer FOREIGN KEY (secondary_transformer_id) REFERENCES transformer(id) ON DELETE NO ACTION;
 
 
 -- drop CONSTRAINT fk_primary_transformer if exists
 ALTER TABLE network DROP CONSTRAINT IF EXISTS fk_primary_transformer;
 
 -- create constraint named fk_primary_transformer on network (transformer_id) referencing transformer (id) avoiding delete
-ALTER TABLE network ADD CONSTRAINT fk_primary_transformer FOREIGN KEY (id) REFERENCES transformer(id) ON DELETE NO ACTION;
+ALTER TABLE network ADD CONSTRAINT fk_primary_transformer FOREIGN KEY (transformer_id) REFERENCES transformer(id) ON DELETE NO ACTION;
 
 
 COMMIT;
